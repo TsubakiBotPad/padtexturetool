9e0b4f455dfb76e87474fa2379a41959f83416c0
AA <chasehult@gmail.com>
2022-12-17 03:57:01 -0700

    Update __init__.py

--- padtexturetool/__init__.py
+++ padtexturetool/__init__.py
@@ -1,8 +1,8 @@
-from extract import do_extract
-from settings import Settings
+from extract import do_extract as _do_extract
+from settings import Settings as _Settings
 
 
 def extract(in_path: str, out_dir: str,
             trimming: bool = True, blackening: bool = True, subtextures: bool = False):
-    settings = Settings(in_path, out_dir, trimming, blackening, subtextures)
-    do_extract(settings)
+    settings = _Settings(in_path, out_dir, trimming, blackening, subtextures)
+    _do_extract(settings)
