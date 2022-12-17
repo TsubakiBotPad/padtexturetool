45c04510f6eecfcdffb10cb8c7f1f13b5754030f
AA <chasehult@gmail.com>
2022-12-17 03:58:12 -0700

    Force settings to be kwargs

--- padtexturetool/__init__.py
+++ padtexturetool/__init__.py
@@ -2,7 +2,7 @@ from extract import do_extract as _do_extract
 from settings import Settings as _Settings
 
 
-def extract(in_path: str, out_dir: str,
+def extract(in_path: str, out_dir: str, *,
             trimming: bool = True, blackening: bool = True, subtextures: bool = False):
     settings = _Settings(in_path, out_dir, trimming, blackening, subtextures)
     _do_extract(settings)
